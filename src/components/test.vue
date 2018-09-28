2e91efcfe186b79bddc66b2596933fd7b0c4f5ee
inling <giraffe.aup@gmail.com>
2018-09-28 14:49:31 +0800

    test-Router
    /test

---
+++ src/components/test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app-test">
+    
+        <h1>测试页面</h1>
+    </div>
+</template>
+
+<script>
+    export default({
+        data(){
+            return {}
+        },
+        methods:{
+
+        },
+        created() {
+            console.log(123);
+        },
+    })
+</script>
+
+<style scoped>
+
+</style>
