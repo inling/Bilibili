cf0fa237068a2635c24021d242914d0336777873
inling <giraffe.aup@gmail.com>
2018-10-16 18:50:51 +0800

    10-16
    食物漂浮

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,4 +10,12 @@
   margin:0;
   padding:0;
 }
+a{
+  text-decoration: none;
+  cursor: pointer;
+}
+.bili-icon{
+  display: inline-block;
+  background-image: url('/img/icons.png');
+}
 </style>
--- client/src/components/Home/HomeHeader.vue
+++ client/src/components/Home/HomeHeader.vue
@@ -6,14 +6,18 @@
             <div class="menuWrapper">
                 <div class="menu-con1">
                     <ul>
-                        <li>主站</li>
-                        <li>画友</li>
-                        <li>音频</li>
-                        <li>游戏中心</li>
-                        <li>直播</li>
-                        <li>会员购</li>
-                        <li>BML</li>
-                        <li>下载APP</li>
+                        <li class="home">
+                            <a><i class="bili-icon"></i>主站</a>
+                        </li>
+                        <li><a>画友</a></li>
+                        <li><a>音频</a></li>
+                        <li><a>游戏中心</a></li>
+                        <li><a>直播</a></li>
+                        <li><a>会员购</a></li>
+                        <li><a>BML</a></li>
+                        <li class="downland">
+                            <a><i class="bili-icon"></i>下载APP</a>
+                        </li>
                     </ul>
                 </div>
                 <div class="menu-con2">
@@ -46,6 +50,7 @@
 </script>
 
 <style scoped>
+
     .appHeader{
         font: 12px Helvetica Neue,Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,WenQuanYi Micro Hei,sans-serif;
     }
@@ -82,10 +87,47 @@
         list-style: none;
         position: relative;
     }
+    
     .appHeader .headerMenu .menuWrapper ul li{
         float: left;
+        position: relative;
+        text-align: center;
         height: 42px;
         line-height: 42px;
+    }
+    .appHeader .headerMenu .menuWrapper ul .home{
+        margin-left: -10px;
+        padding-left: 12px;
+    }
+    .appHeader .headerMenu .menuWrapper ul li a{
+        display: block;
+        height: 100%;
+        padding: 0 8px;
+    }
+    .appHeader .headerMenu .menuWrapper ul .home a{
+        padding-left: 20px;
+    }
+    .appHeader .headerMenu .menuWrapper ul .home a i{
+        position: absolute;
+        display: inline-block;
+        width: 17px;
+        height:14px;
+        top:12px;
+        left:10px;
+        background-position: -919px -88px;
+    }
+    .appHeader .headerMenu .menuWrapper ul .downland{
+        padding: 0 10px;
+    }
+    .appHeader .headerMenu .menuWrapper ul .downland a{
+        padding: 0;
+    }
+    .appHeader .headerMenu .menuWrapper ul .downland a i{
+        display: inline-block;
+        width: 16px;
+        height:21px;
+        background-position: -1371px -1175px;
+        vertical-align: middle;
     }
     .appHeader .headerBanner{
         position: relative;
