486dd36e666a3b618213881d3b7253229ae80ab3
inling.aup <43487924+inling@users.noreply.github.com>
2018-10-23 01:20:56 +0800

    10-23
    二维码

--- client/src/components/Home/subCom_header/topNav.vue
+++ client/src/components/Home/subCom_header/topNav.vue
@@ -115,5 +115,6 @@
     .topNav .menuWrapper .menu-con3{
         float: right;
     }
+
 </style>
 
--- client/src/components/Home/subCom_header/ul1-style/ul1-li-style3.vue
+++ client/src/components/Home/subCom_header/ul1-style/ul1-li-style3.vue
@@ -1,9 +1,11 @@
 <template>
-    <li title="下载APP" class="ul1-li-style3 headli">
+    <li title="下载APP" class="ul1-li-style3 headli" @mouseover="setMouseOver" @mouseout="setMouseOut">
         <router-link to="#" class="heada">
-            <i class="bili-icon"></i>
-                下载APP
+            <i class="bili-icon"></i>下载APP
         </router-link>
+        <transition name="box-show">
+            <div class="app-orcode-box" v-show="isShowBox"></div>
+        </transition>
     </li>
 </template>
 
@@ -11,11 +13,16 @@
     export default({
         data(){
             return{
-
+                isShowBox:false
             }
         },
         methods:{
-
+            setMouseOver(){
+                this.isShowBox=true;
+            },
+            setMouseOut(){
+                this.isShowBox=false;
+            }
         },
         created() {
             
@@ -40,6 +47,38 @@
         height:21px;
         background-position: -1371px -1175px;
         vertical-align: middle;
+    }
+    .ul1-li-style3 .app-orcode-box{
+        position: absolute;
+        background: red;
+        left: -20px;
+        top: 42px;
+        width: 259px;
+        height: 174px;
+        background: url(/img/header/orcode-frame.png);
+    }
+    .app-orcode-box::before{
+        content: "";
+        position: absolute;
+        width: 97px;
+        height: 97px;
+        left: 82px;
+        top: 30px;
+        background: url(/img/header/erW.png);
+    }
+    .box-show-enter-active{
+        transition: all .3s ease;
+    }
+    .box-show-leave-active{
+        transition: all .3s ease;
+    }
+    .box-show-enter{
+        margin-top: 10px;
+        opacity: 0;
+    }
+    .box-show-leave-to{
+        margin-top: 10px;
+        opacity: 0;
     }
 </style>
 
