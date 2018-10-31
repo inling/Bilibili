64f2d6ce61f03614039c544b62ba26112383a8cf
inling <giraffe.aup@gmail.com>
2018-10-31 21:06:27 +0800

    10 31

--- server/app.js
+++ server/app.js
@@ -5,8 +5,9 @@ const app=express();
 /*路由 */
 var example=require('./routes/example')
 
+app.use(express.static('public'))
 //监听端口
-app.listen(3000);
+app.listen(80);
 
 
 app.use('/example',example);
